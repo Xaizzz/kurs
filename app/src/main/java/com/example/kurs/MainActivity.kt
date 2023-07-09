7a1343a898eddafdc1720302fe7ac11e63d06205
Xaizzz <ilia.epifanov@yandex.ru>
2023-07-09 20:19:08 +0300

    1

--- app/src/main/java/com/example/kurs/MainActivity.kt
+++ app/src/main/java/com/example/kurs/MainActivity.kt
@@ -3,37 +3,65 @@ package com.example.kurs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.TextureView
 import android.view.View
+import android.widget.TextView
 import com.example.kurs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass : ActivityMainBinding
-    val a = 324
-    val b = 34
+    lateinit var bindingClass: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        bindingClass.a.text = "a"
-        bindingClass.b.text = "b"
-        bindingClass.b1.setOnClickListener {
-            bindingClass.result.text = (a + b).toString()
+        bindingClass.button.setOnClickListener {
+            if (bindingClass.edValue.text.toString()
+                    .isNotEmpty()
+            ) {
+                val value = bindingClass.edValue.text.toString().toInt()
 
-        }
-        bindingClass.b2.setOnClickListener {
-            bindingClass.result.text = (a - b).toString()
-        }
-        bindingClass.b3.setOnClickListener {
-            bindingClass.result.text = (a * b).toString()
-        }
-        bindingClass.b4.setOnClickListener {
-            bindingClass.result.text = (a / b).toString()
-        }
+                when (value) {
+                    in 1..10000 -> {
+                        bindingClass.result.text = "Начинающий"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                    in 10001..100000 -> {
+                        bindingClass.result.text = "Любитель"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                    in 100001..500000 -> {
+                        bindingClass.result.text = "Популярный"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                    in 500001..1000000 -> {
+                        bindingClass.result.text = "Очень популярный"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                    in 1000001..100000000 -> {
+                        bindingClass.result.text = "Звезда"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                    else -> {
+                        bindingClass.result.text = "Ваше число не попадает в рамки"
+                        bindingClass.result.visibility = View.VISIBLE
+                    }
+
+                }
 
 
+            } else {
+                bindingClass.result.text = "Ваше число не попадает в рамки"
+                bindingClass.result.visibility = View.VISIBLE
+            }
         }
-
-
     }
+}
 
 
