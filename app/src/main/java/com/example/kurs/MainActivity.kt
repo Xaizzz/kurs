6f990f6ab1eddf3fc97f75a4953acdfee5b11e01
Xaizzz <ilia.epifanov@yandex.ru>
2023-07-14 20:52:46 +0300

    ActivityResult

--- app/src/main/java/com/example/kurs/MainActivity.kt
+++ app/src/main/java/com/example/kurs/MainActivity.kt
@@ -1,34 +1,39 @@
 package com.example.kurs
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.kurs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivityMainBinding
-
+    private var launcher: ActivityResultLauncher<Intent>? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        bindingClass.bAct2.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val text = result.data?.getStringExtra("key")
+                    bindingClass.textView.text = text
+                }
+            }
+        bindingClass.button.setOnClickListener {
+            launcher?.launch(Intent(this, MainActivity2::class.java))
         }
-        bindingClass.bAct3.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-
-        }
-
-
 
     }
+
+
 }
 
 
--- app/src/main/java/com/example/kurs/MainActivity2.kt
+++ app/src/main/java/com/example/kurs/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.kurs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kurs.databinding.ActivityMain2Binding
@@ -7,8 +8,16 @@ import com.example.kurs.databinding.ActivityMain2Binding
 class MainActivity2 : AppCompatActivity() {
     lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super .onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.button2.setOnClickListener {
+            val i = Intent()
+            i.putExtra("key", binding.editTextText2.text.toString())
+            setResult(RESULT_OK, i)
+            finish()
+        }
+
+
     }
 }
