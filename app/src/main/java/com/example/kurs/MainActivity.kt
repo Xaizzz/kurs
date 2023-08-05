60c8253bdf8af0773a8d218c7d982fc2c106d55a
Xaizzz <ilia.epifanov@yandex.ru>
2023-08-05 13:56:21 +0300

    list

--- app/src/main/java/com/example/kurs/MainActivity.kt
+++ app/src/main/java/com/example/kurs/MainActivity.kt
@@ -12,20 +12,47 @@ import com.example.kurs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bindingClass: ActivityMainBinding
-    private var login: String = "empty"
-    private var password: String = "empty"
-    private var name: String = "empty"
-    private var name2: String = "empty"
-    private var name3: String = "empty"
-    private var avatarImageId: Int = 0
-    private var signUpLauncher: ActivityResultLauncher<Intent>? = null
-    private var signInLauncher: ActivityResultLauncher<Intent>? = null
+    val lostArray = arrayListOf<Int>(10000, 20000, 30000, 5000, 120, 250)
+    val earnArray = arrayListOf<Int>(30000, 60000, 8000, 200, 1220, 3230)
+    val resultArray = arrayListOf<String>("", "", "", "", "", "")
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        val names = resources.getStringArray(R.array.names)
+        for (i in 0..5) {
+            resultArray[i] = (earnArray[i] - lostArray[i]).toString() + " " + names[i]
+        }
+        bindingClass.button.setOnClickListener {
+          when(bindingClass.edT.text.toString()){
+              names[0] -> {
+                  bindingClass.textView.text = resultArray[0]
+              }
+              names[1] -> {
+                    bindingClass.textView.text = resultArray[1]
+              }
+              names[2] -> {
+                  bindingClass.textView.text = resultArray[2]
+              }
+              names[3] -> {
+                  bindingClass.textView.text = resultArray[3]
+              }
+              names[4] -> {
+                  bindingClass.textView.text = resultArray[4]
+              }
+              names[5] -> {
+                  bindingClass.textView.text = resultArray[5]
+              }
+              else -> {
+                  bindingClass.textView.text = "Такого имени нет"}
+          }
 
 
-}}
+
+
+        }
+    }
+}
